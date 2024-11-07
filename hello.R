18e79317d59bfd7394c8f05a2727a809e811cd0d
Maria Eduarda G <mariaeadornes@gmail.com>
2024-11-07 12:19:55 -0300

    testes

--- hello.R
+++ hello.R
@@ -3,9 +3,11 @@ hello
 library(usethis)
 use_git()
 
+
 use_git_config(user.name = "Maria Eduarda G", user.email = "mariaeadornes@gmail.com")
 
-use_readme_md()
-
-
 #download laba
+
+#aaaaaaaaaaaaaa
+
+#testes testsrstsesfd tsr
